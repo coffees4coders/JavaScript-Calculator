0766a685cbd735d3a9fffefdfedcb3c4552b6a39
Jay Ullman <jayullman@gmail.com>
2016-10-30 19:59:33 -0700

    WIP: work on addition functionality

--- calc.js
+++ calc.js
@@ -1,29 +1,42 @@
 // this will hold the numbers selected by
 // the calculator number buttons before
 // an operator button is pressed
-var numsArr = [];
 
 // this will hold the individual inputs
 // to be operated on
 var inputArr = [];
+var smallDisplayArr = [];
+var operationsArr = [];
 
-function updateDisplay(item) {
+// takes button input from user
+// updates readout display at end of function
+function processInput(item) {
 
-
-  var readout = document.getElementById('calc-screen');
+  var mainReadout = document.getElementById('main-readout');
+  var smallReadout = document.getElementById('small-readout');
 
   // if parameter is a number character, add to numsArr
   if (/\w/g.test(item)) {
-    numsArr.push(item);
-  }
-  // TODO: finish this
-  if (item === '+') {
-    console.log('+ clicked');
-    console.log(item.innerHTML);
-    inputArr.push(numsArr)
-    numsArr.push(' + ');
-  }
+    inputArr.push(item);
+    // update readout display
+    // if statement clears display if +
+    // was entered previously
 
+    // FIXME: this doesn't work!
+    if (inputArr[inputArr.length-1] === "+") {
+        mainReadout.innerHTML = "";
+    }
+    mainReadout.innerHTML = inputArr.join('');
+  }
+  // IDEA: make else into switch
+
+  if (item === '+') {
+    inputArr.push(inputArr)
+    inputArr.push(' + ');
+    smallDisplayArr.push(inputArr);
+    mainReadout.innerHTML = "";
+    smallReadout.innerHTML = smallDisplayArr;
+  }
 
   // if function receives 'clear' string from
   // clear button click, clear the numsArr
@@ -31,9 +44,6 @@ function updateDisplay(item) {
     numsArr = [];
     readout.innerHTML = "";
   }
-
-  // update readout display
-  readout.innerHTML = numsArr.join('');
 };
 
 window.onload = function() {
@@ -45,19 +55,19 @@ window.onload = function() {
   for (var i = 0; i < numberButtons.length; i++) {
     numberButtons[i].addEventListener('click', function(e) {
       // what heppens when you click on a number button
-      updateDisplay(e.target.innerHTML); // sends input to updateDisplay
+      processInput(e.target.innerHTML); // sends input to processInput()
     });
   }
-  // attach onclick event listeners to all operator buttons
   for (var i = 0; i < operatorButtons.length; i++) {
     operatorButtons[i].addEventListener('click', function(e) {
+        // attach onclick event listeners to all operator buttons
       // what heppens when you click on an operator button
-      updateDisplay(e.target.innerHTML);
+      processInput(e.target.innerHTML);
     });
   }
 
   clearButton[0].addEventListener('click', function() {
-    updateDisplay('clear');
+    processInput('clear');
   });
 
 };
